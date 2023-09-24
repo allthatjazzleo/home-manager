78d3eaf61af6eefe6f77254a242f1eac4da974bf
allthatjazzleo <pangleo1994@gmail.com>
2023-09-24 15:29:12 +0800

    enable JavaFX by override

--- cells/homemanager/programs/java.nix
+++ cells/homemanager/programs/java.nix
@@ -5,10 +5,11 @@
   ...
 }: let
   inherit (inputs) nixpkgs;
+  jdk17 = nixpkgs.jdk17.override {enableJavaFX = true;};
 in {
   java = {
     enable = user.java or false;
     # nixpkgs.jdk17 should enable enableJavaFX
-    package = nixpkgs.jdk17.overrideAttrs (oldAttrs: {enableJavaFX = true;});
+    package = jdk17;
   };
 }
